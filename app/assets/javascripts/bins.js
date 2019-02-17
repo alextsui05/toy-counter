f2a9df3fc7f88fd0dd6a78dca99394a0be559537
Alex Tsui <alextsui05@gmail.com>
2019-02-17 05:01:38 +0000

    Add a simple XHR example

--- app/assets/javascripts/bins.js
+++ app/assets/javascripts/bins.js
@@ -1,5 +1,30 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 document.addEventListener('turbolinks:load', function() {
-  console.log('Hello, world!');
+  var println = function(o) {
+    console.log(o);
+    consoleElement = document.getElementById('console');
+    consoleElement.innerHTML += "<br />" + o;
+  };
+
+  var get = function(url, done) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', binsPath);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onload = function() {
+      done(null, xhr.response);
+    };
+    xhr.onerror = function() {
+      done(xhr.response);
+    };
+    xhr.send();
+  };
+
+  println('Hello, world!');
+  var dataElement = document.getElementById('data');
+  var binsPath = dataElement.getAttribute('data-bins-path');
+  get(binsPath, function(err, data) {
+    if (err) { throw err; }
+    println(data);
+  });
 });
